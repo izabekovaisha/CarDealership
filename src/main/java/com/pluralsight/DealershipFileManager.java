439f7e0c5d5426ed0d110cd4de4d73c8cb8c2b1d
izabekovaisha <izabekovaisha@gmail.com>
2024-05-12 14:14:37 -0400

    Add Dealership getDealership and saveDealership methods

--- src/main/java/com/pluralsight/DealershipFileManager.java
+++ src/main/java/com/pluralsight/DealershipFileManager.java
@@ -1,59 +1,75 @@
 package com.pluralsight;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class DealershipFileManager {
 
-    private static ArrayList<Dealership> dealership = new ArrayList<>();
-    private static String filename = "dealership.csv";
+    private static final String filename = "dealership.csv";
 
-
-    public Dealership getDealership(String filename) {
-        List<Dealership>  dealership = new ArrayList<>();
+/* Method to load and read the dealership.csv file
+* Using Dealership object, BufferedReader, and parsing
+* It also populates the inventory of vehicles
+*/
+    public static Dealership getDealership() {
+        Dealership dealership = null;
+        List<Vehicle> inventory = new ArrayList<>();
 
         try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(filename));
-            String line;
+            String line1;
             String line2 = null;
 
-            if ((line = bufferedReader.readLine()) != null) {
-                    String[] parts = line.split("\\|");
+            while ((line1 = bufferedReader.readLine()) != null) {
+                String[] parts = line1.split("\\|");
 
-                    if (parts.length == 3) {
-                        String name = parts[0].trim();
-                        String address = parts[1].trim();
-                        String phone = parts[2].trim();
+                if (parts.length == 3) {
+                    String name = parts[0].trim();
+                    String address = parts[1].trim();
+                    String phone = parts[2].trim();
+                    dealership = new Dealership(name, address, phone);
+                } else if (line2 != null && !line1.equals(line2)) {
+                    parts = line2.split("\\|");
+                    if (parts.length == 8) {
+                        int vin = Integer.parseInt(parts[0].trim());
+                        int year = Integer.parseInt(parts[1].trim());
+                        String make = parts[2].trim();
+                        String model = parts[3].trim();
+                        String vehicleType = parts[4].trim();
+                        String color = parts[5].trim();
+                        int odometer = Integer.parseInt(parts[6].trim());
+                        double price = Double.parseDouble(parts[7].trim());
 
+                        inventory.add(new Vehicle(vin, year, make, model, vehicleType, color, odometer, price));
                     }
                 }
-
-            while ((line2 = bufferedReader.readLine()) != null) {
-                String[] parts = line2.split("\\|");
-
-                if (parts.length == 8) {
-                    int vin = Integer.parseInt(parts[0].trim());;
-                    int year = Integer.parseInt(parts[1].trim());;
-                    String make = parts[2].trim();;
-                    String model = parts[3].trim();;
-                    String vehicleType = parts[4].trim();;
-                    String color = parts[5].trim();;
-                    int odometer = Integer.parseInt(parts[6].trim());;
-                    double price = Double.parseDouble(parts[7].trim());
-
-            inventory.add(new Vehicle(vin, year, make, model, vehicleType, color, odometer, price)));
-                }
+                line2 = line1;
             }
-            bufferedReader.close();
-
-        } catch (Exception e) {
+        } catch (IOException e) {
             System.out.println("Error loading inventory: " + e.getMessage());
         }
+        return dealership;
+    }
+
+    // Method to overwrite the dealership.csv file with current dealership information and inventory list
+    public static void saveDealership(Dealership dealership) {
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(filename, true));
+            writer.write(getDealership().getName() + "|" + getDealership().getAddress() + "|" + getDealership().getPhone());
+            writer.newLine();
+            writer.close();
+
+            for (Vehicle vehicle : getDealership().getAllVehicles()) {
+                writer.write(vehicle.getVin() + "|" + vehicle.getYear() + "|" + vehicle.getYear() + "|" + vehicle.getMake() + "|" + vehicle.getModel() + "|" + vehicle.getVehicleType() + "|" + vehicle.getColor() + "|" + vehicle.getOdometer() + "|" + vehicle.getPrice());
+                writer.newLine();
+                writer.close();
+            }
+        } catch (IOException e) {
+            System.err.println(("Error writing dealership information: " + e.getMessage()));
+        }
+    }
     }
 
 
 
-    public saveDealership()
-}
